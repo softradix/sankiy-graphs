a3d73488e348a8cfe9e4768ea856aadaccd8f896
DamanSoftRadixx <143387820+DamanSoftRadixx@users.noreply.github.com>
2023-09-28 19:04:44 +0530

    Before HiChat

---
+++ lib/main.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:hichartdemo/views/sankey_diagram.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body:SizedBox(
+        width: double.infinity,
+        height: double.infinity,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            ElevatedButton(onPressed: () {
+              Navigator.push(context,MaterialPageRoute(builder: (context) =>SankeyDiagram()));
+            }, child: const Text('Sankey Diagram Chat'))
+          ],
+        ),
+      ),
+    );
+  }
+}
+
---
+++ lib/views/sankey_diagram.dart
@@ -0,0 +1,125 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter/services.dart';
+import 'dart:io' show Platform;
+
+class SankeyDiagram extends StatefulWidget {
+  const SankeyDiagram({super.key});
+
+  @override
+  State<SankeyDiagram> createState() => _SankeyDiagramState();
+}
+
+class _SankeyDiagramState extends State<SankeyDiagram> {
+  var android = Platform.isAndroid;
+  var ios = Platform.isIOS;
+
+  // This is used in the platform side to register the view.
+  final String viewType = '<platform-view-type-sankey-diagram>';
+// Pass parameters to the platform side.
+  final Map<String, dynamic> creationParams = <String, dynamic>{"daman": "Testing"};
+  GlobalKey _platformViewKey = GlobalKey();
+  bool? rebuild;
+  @override
+  Widget build(BuildContext context) {
+    final platform = Theme.of(context).platform;
+    if (rebuild != null) {
+      _platformViewKey = GlobalKey();
+      rebuild = null;
+    }
+    rebuild ??= true;
+    var size = MediaQuery.of(context).size;
+    switch (platform) {
+      case TargetPlatform.android:
+        return Scaffold(
+            body: SafeArea(
+          child: Container(
+              width: size.width,
+              height: size.height,
+              color: Colors.white,
+              child: Stack(fit: StackFit.expand, children: [
+                loadingWidget(),
+                androidNativeView()
+              ])),
+        ));
+
+      case TargetPlatform.iOS:
+        return Scaffold(
+            body: SafeArea(
+          child: Container(
+              width: size.width,
+              height: size.height,
+              child: Stack(
+                fit: StackFit.expand,
+                children: [
+                  loadingWidget(),
+                  iosNativeView()
+                ],
+              )),
+        ));
+      default:
+        return Container(
+          color: Colors.white,
+          alignment: Alignment.center,
+          child: const Text('Target device not support.'),
+        );
+    }
+  }
+
+  Widget loadingWidget(){
+    return const Center(
+      child: Text('Loading....',style: TextStyle(color: Colors.black,fontSize: 18,fontWeight: FontWeight.normal),),
+    );
+  }
+
+  // For ios native view call/return
+  UiKitView iosNativeView(){
+    return UiKitView(
+      viewType: viewType,
+      layoutDirection: TextDirection.ltr,
+      creationParams: creationParams,
+      creationParamsCodec: const StandardMessageCodec(),
+    );
+  }
+  // For android native view call/return
+  PlatformViewLink androidNativeView(){
+    return PlatformViewLink(
+      key: _platformViewKey,
+      viewType: viewType,
+      surfaceFactory: (context, controller) {
+        return AndroidViewSurface(
+          controller: controller as AndroidViewController,
+          gestureRecognizers: const <Factory<
+              OneSequenceGestureRecognizer>>{},
+          hitTestBehavior: PlatformViewHitTestBehavior.opaque,
+        );
+      },
+      onCreatePlatformView: (params) {
+        return PlatformViewsService.initSurfaceAndroidView(
+          id: params.id,
+          viewType: viewType,
+          layoutDirection: TextDirection.ltr,
+          creationParams: creationParams,
+          creationParamsCodec: const StandardMessageCodec(),
+          onFocus: () {
+            params.onFocusChanged(true);
+          },
+        )
+          ..addOnPlatformViewCreatedListener(
+              params.onPlatformViewCreated)
+          ..create();
+      },
+    );
+    /*
+            // keyboard handling and accessibility features might not work /.
+             AndroidView(
+                   viewType: viewType,
+                   layoutDirection: TextDirection.ltr,
+                   creationParams: creationParams,
+                   creationParamsCodec: const StandardMessageCodec(),
+                 )*/
+  }
+}
+
