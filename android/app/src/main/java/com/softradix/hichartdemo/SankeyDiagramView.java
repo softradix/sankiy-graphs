65b89086b8975686c47f810dafc99814e284a010
DamanSoftRadixx <143387820+DamanSoftRadixx@users.noreply.github.com>
2023-10-04 16:48:27 +0530

    some changes

--- android/app/src/main/java/com/softradix/hichartdemo/SankeyDiagramView.java
+++ android/app/src/main/java/com/softradix/hichartdemo/SankeyDiagramView.java
@@ -4,19 +4,22 @@ import android.content.Context;
 import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.LinearLayout;
-
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-
+import com.highsoft.highcharts.common.HIColor;
+import com.highsoft.highcharts.common.hichartsclasses.HINodes;
 import com.highsoft.highcharts.common.hichartsclasses.HIOptions;
 import com.highsoft.highcharts.common.hichartsclasses.HISankey;
 import com.highsoft.highcharts.common.hichartsclasses.HITitle;
+import com.highsoft.highcharts.common.hichartsclasses.HITooltip;
 import com.highsoft.highcharts.core.HIChartView;
-
+import com.highsoft.highcharts.core.HIFunction;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
+
+import io.flutter.plugin.platform.PlatformView;
 
 import io.flutter.plugin.platform.PlatformView;
 
@@ -25,8 +28,8 @@ class SankeyDiagramView implements PlatformView {
     private HIChartView hIChartView;
 
     SankeyDiagramView(@NonNull Context context, int id, @Nullable Map<String, Object> creationParams) {
-        // for add Chat to view.
-        addSankeyChat(context);
+        // for add Chart to view.
+        addSankeyChart(context,creationParams);
     }
 
     @NonNull
@@ -37,67 +40,36 @@ class SankeyDiagramView implements PlatformView {
     @Override
     public void dispose() {
     }
-    void addSankeyChat(Context context){
+    void addSankeyChart(Context context, Map<String, Object> creationParams){
+        String title = "Chart";
+        String seriesName = "Chart";
+        List<List<Object>> dataList = new ArrayList<>();
+        int [] arr=new int [4];
+        if(creationParams.containsKey("title")){
+            title = (String) creationParams.get("title");
+        }
+        if(creationParams.containsKey("seriesName")){
+            seriesName = (String) creationParams.get("seriesName");
+        }
+        if(creationParams.containsKey("dataList")){
+            List<List<Object>> list = ( List<List<Object>> ) creationParams.get("dataList");
+            dataList.addAll(list);
+        }
         hIChartView = new HIChartView(context);
+        hIChartView.theme= "brand-dark";
         hIChartView.showLoading("Loading....");
         hIChartView.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
         hIChartView.setBackgroundColor(context.getResources().getColor(android.R.color.transparent));
         hIChartView.plugins = new ArrayList<>(Arrays.asList("sankey"));
         HIOptions options = new HIOptions();
-        HITitle title = new HITitle();
-        title.setText("Highcharts Sankey Diagram");
-        options.setTitle(title);
-        HISankey series1 = new HISankey();
+        HITitle titlee = new HITitle();
+        titlee.setText(title);
+        options.setTitle(titlee);
         String[] keys = new String[]{"from", "to", "weight"};
+        HISankey series1 = new HISankey();
         series1.setKeys(new ArrayList<>(Arrays.asList(keys)));
-        series1.setName("Sankey demo series");
-        Object[] object1 = new Object[]{"Brazil", "Portugal", 5};
-        Object[] object2 = new Object[]{"Brazil", "France", 1};
-        Object[] object3 = new Object[]{"Brazil", "Spain", 1};
-        Object[] object4 = new Object[]{"Brazil", "England", 1};
-        Object[] object5 = new Object[]{"Canada", "Portugal", 1};
-        Object[] object6 = new Object[]{"Canada", "France", 5};
-        Object[] object7 = new Object[]{"Canada", "England", 1};
-        Object[] object8 = new Object[]{"Mexico", "Portugal", 1};
-        Object[] object9 = new Object[]{"Mexico", "France", 1};
-        Object[] object10 = new Object[]{"Mexico", "Spain", 5};
-        Object[] object11 = new Object[]{"Mexico", "England", 1};
-        Object[] object12 = new Object[]{"USA", "Portugal", 1};
-        Object[] object13 = new Object[]{"USA", "France", 1};
-        Object[] object14 = new Object[]{"USA", "Spain", 1};
-        Object[] object15 = new Object[]{"USA", "England", 5};
-        Object[] object16 = new Object[]{"Portugal", "Angola", 2};
-        Object[] object17 = new Object[]{"Portugal", "Senegal", 1};
-        Object[] object18 = new Object[]{"Portugal", "Morocco", 1};
-        Object[] object19 = new Object[]{"Portugal", "South Africa", 3};
-        Object[] object20 = new Object[]{"France", "Angola", 1};
-        Object[] object21 = new Object[]{"France", "Senegal", 3};
-        Object[] object22 = new Object[]{"France", "Mali", 3};
-        Object[] object23 = new Object[]{"France", "Morocco", 3};
-        Object[] object24 = new Object[]{"France", "South Africa", 1};
-        Object[] object25 = new Object[]{"Spain", "Senegal", 1};
-        Object[] object26 = new Object[]{"Spain", "Morocco", 3};
-        Object[] object27 = new Object[]{"Spain", "South Africa", 1};
-        Object[] object28 = new Object[]{"England", "Angola", 1};
-        Object[] object29 = new Object[]{"England", "Senegal", 1};
-        Object[] object30 = new Object[]{"England", "Morocco", 2};
-        Object[] object31 = new Object[]{"England", "South Africa", 7};
-        Object[] object32 = new Object[]{"South Africa", "China", 5};
-        Object[] object33 = new Object[]{"South Africa", "India", 1};
-        Object[] object34 = new Object[]{"South Africa", "Japan", 3};
-        Object[] object35 = new Object[]{"Angola", "China", 5};
-        Object[] object36 = new Object[]{"Angola", "India", 1};
-        Object[] object37 = new Object[]{"Angola", "Japan", 3};
-        Object[] object38 = new Object[]{"Senegal", "China", 5};
-        Object[] object39 = new Object[]{"Senegal", "India", 1};
-        Object[] object40 = new Object[]{"Senegal", "Japan", 3};
-        Object[] object41 = new Object[]{"Mali", "China", 5};
-        Object[] object42 = new Object[]{"Mali", "India", 1};
-        Object[] object43 = new Object[]{"Mali", "Japan", 3};
-        Object[] object44 = new Object[]{"Morocco", "China", 5};
-        Object[] object45 = new Object[]{"Morocco", "India", 1};
-        Object[] object46 = new Object[]{"Morocco", "Japan", 3};
-        series1.setData(new ArrayList<>(Arrays.asList(object1, object2, object3, object4, object5, object6, object7, object8, object9, object10, object11, object12, object13, object14, object15, object16, object17, object18, object19, object20, object21, object22, object23, object24, object25, object26, object27, object28, object29, object30, object31, object32, object33, object34, object35, object36, object37, object38, object39, object40, object41, object42, object43, object44, object45, object46)));
+        series1.setName(seriesName);
+        series1.setData(new ArrayList<>(dataList));
         options.setSeries(new ArrayList<>(Arrays.asList(series1)));
         hIChartView.setOptions(options);
     }
--- lib/views/sankey_diagram.dart
+++ lib/views/sankey_diagram.dart
@@ -19,11 +19,12 @@ class _SankeyDiagramState extends State<SankeyDiagram> {
   // This is used in the platform side to register the view.
   final String viewType = '<platform-view-type-sankey-diagram>';
 // Pass parameters to the platform side.
-  final Map<String, dynamic> creationParams = <String, dynamic>{"daman": "Testing"};
+  final Map<String, dynamic> creationParams = <String, dynamic>{"title": "Highcharts Sankey Diagram Daman","seriesName":"Flow"};
   GlobalKey _platformViewKey = GlobalKey();
   bool? rebuild;
   @override
   Widget build(BuildContext context) {
+    creationParams['dataList']=dataList();
     final platform = Theme.of(context).platform;
     if (rebuild != null) {
       _platformViewKey = GlobalKey();
@@ -35,30 +36,30 @@ class _SankeyDiagramState extends State<SankeyDiagram> {
       case TargetPlatform.android:
         return Scaffold(
             body: SafeArea(
-          child: Container(
-              width: size.width,
-              height: size.height,
-              color: Colors.white,
-              child: Stack(fit: StackFit.expand, children: [
-                loadingWidget(),
-                androidNativeView()
-              ])),
-        ));
+              child: Container(
+                  width: size.width,
+                  height: size.height,
+                  color: Colors.white,
+                  child: Stack(fit: StackFit.expand, children: [
+                    loadingWidget(),
+                    androidNativeView()
+                  ])),
+            ));
 
       case TargetPlatform.iOS:
         return Scaffold(
             body: SafeArea(
-          child: Container(
-              width: size.width,
-              height: size.height,
-              child: Stack(
-                fit: StackFit.expand,
-                children: [
-                  loadingWidget(),
-                  iosNativeView()
-                ],
-              )),
-        ));
+              child: Container(
+                  width: size.width,
+                  height: size.height,
+                  child: Stack(
+                    fit: StackFit.expand,
+                    children: [
+                      loadingWidget(),
+                      iosNativeView()
+                    ],
+                  )),
+            ));
       default:
         return Container(
           color: Colors.white,
@@ -73,7 +74,6 @@ class _SankeyDiagramState extends State<SankeyDiagram> {
       child: Text('Loading....',style: TextStyle(color: Colors.black,fontSize: 18,fontWeight: FontWeight.normal),),
     );
   }
-
   // For ios native view call/return
   UiKitView iosNativeView(){
     return UiKitView(
@@ -83,6 +83,7 @@ class _SankeyDiagramState extends State<SankeyDiagram> {
       creationParamsCodec: const StandardMessageCodec(),
     );
   }
+
   // For android native view call/return
   PlatformViewLink androidNativeView(){
     return PlatformViewLink(
@@ -112,7 +113,58 @@ class _SankeyDiagramState extends State<SankeyDiagram> {
           ..create();
       },
     );
-
+  }
+  List<dynamic> dataList() {
+    List<List<Object>> dataList = [
+      ["Brazil", "Portugal", 5],
+      ["Brazil", "France", 1],
+      ["Brazil", "Spain", 1],
+      ["Brazil", "England", 1],
+      ["Canada", "Portugal", 1],
+      ["Canada", "France", 5],
+      ["Canada", "England", 1],
+      ["Mexico", "Portugal", 1],
+      ["Mexico", "France", 1],
+      ["Mexico", "Spain", 5],
+      ["Mexico", "England", 1],
+      ["USA", "Portugal", 1],
+      ["USA", "France", 1],
+      ["USA", "Spain", 1],
+      ["USA", "England", 5],
+      ["Portugal", "Angola", 2],
+      ["Portugal", "Senegal", 1],
+      ["Portugal", "Morocco", 1],
+      ["Portugal", "South Africa", 3],
+      ["France", "Angola", 1],
+      ["France", "Senegal", 3],
+      ["France", "Mali", 3],
+      ["France", "Morocco", 3],
+      ["France", "South Africa", 1],
+      ["Spain", "Senegal", 1],
+      ["Spain", "Morocco", 3],
+      ["Spain", "South Africa", 1],
+      ["England", "Angola", 1],
+      ["England", "Senegal", 1],
+      ["England", "Morocco", 2],
+      ["England", "South Africa", 7],
+      ["South Africa", "China", 5],
+      ["South Africa", "India", 1],
+      ["South Africa", "Japan", 3],
+      ["Angola", "China", 5],
+      ["Angola", "India", 1],
+      ["Angola", "Japan", 3],
+      ["Senegal", "China", 5],
+      ["Senegal", "India", 1],
+      ["Senegal", "Japan", 3],
+      ["Mali", "China", 5],
+      ["Mali", "India", 1],
+      ["Mali", "Japan", 3],
+      ["Morocco", "China", 5],
+      ["Morocco", "India", 1],
+      ["Morocco", "Japan", 3],
+    ];
+    return dataList;
   }
 }
+
 
